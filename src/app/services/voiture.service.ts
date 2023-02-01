a40ae8f16597270107fe579385d551a8fb9bfcf3
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-02-01 04:22:18 +0000

    Commit 10 = Revues et apports de modification au component modele, Implémentation de la liste des voitures et leur ajout

--- src/app/services/voiture.service.ts
+++ src/app/services/voiture.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { Voiture } from '../models/voiture';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoitureService {
 
-  constructor() { }
+  public voiture: Voiture = new Voiture();
+  url!: string;
+
+  constructor(private httpClient: HttpClient) {
+    const APIEndpoint = environment.APIEndpoint;
+    this.url = APIEndpoint + 'api/';
+  }
+
+  //Ajout d'une occurrence de voiture;
+  //url: http://localhost:9002/api/voiture/ajouter
+  addVoiture(v: Voiture): Observable<Voiture>{
+    return this.httpClient.post<Voiture>(this.url + 'voiture/ajouter', v);
+  }
+
+  //Modification d'une occurrence de voiture;
+  //url: http://localhost:9002/api/voiture/modifier
+  updateVoiture(c: Voiture): Observable<Voiture>{
+    return this.httpClient.put<Voiture>(this.url + 'voiture/modifier', c);
+  }
+
+  //Suppression d'une occurrence de voiture par la clé primaire ;
+  //url: http://localhost:9002/api/voiture/supprimer/{id}
+  deleteById(id: number){
+    return this.httpClient.delete(this.url + 'voiture/supprimer/' + id);
+  }
+
+  //Affichage de toutes les occurrences de voitures;
+  //url: http://localhost:9002/api/voitures
+  getAll(): Observable<Array<Voiture>>{
+    return this.httpClient.get<Array<Voiture>>(this.url + 'voitures');
+  }
+
+  //Recherche d'une occurrence de voiture par la clé primaire ;
+  //url: http://localhost:9002/api/voiture/{id}
+  findById(id: number): Observable<Voiture>{
+    return this.httpClient.get<Voiture>(this.url + 'voiture/' + id);
+  }
+
+  //Affichage du nombre d'occurrences de voiture.
+  //url: http://localhost:9002/api/voiture/count
+  count(): Observable<any>{
+    return this.httpClient.get<any>(this.url + 'voiture/count');
+  };
 }
--- src/app/voiture/voiture.component.ts
+++ src/app/voiture/voiture.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ModeleService } from '../services/modele.service';
+import { MarqueService } from '../services/marque.service';
+import { VoitureService } from '../services/voiture.service';
+import { Voiture } from '../models/voiture';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Marque } from '../models/marque';
+import { Modele } from '../models/modele';
 
 @Component({
   selector: 'app-voiture',
@@ -7,9 +14,144 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoitureComponent implements OnInit {
 
-  constructor() { }
+  voirListesVoitures: boolean = true;
+  voirFormulaireAjout: boolean = false;
+  voirFormulaireModification: boolean = true;
+
+  voirPageDetail: boolean = true;
+
+  erreur: boolean = true;
+  voiture = this.voitureService.voiture;
+  marques : Marque[] = [];
+  modeles : Modele[] = [];
+  voitures : Voiture[] = [];
+  messageErreur: string = "";
+
+  constructor(private modeleService: ModeleService,
+    private marqueService: MarqueService,
+    private voitureService: VoitureService) { }
+
+  voitureForm: any;
 
   ngOnInit(): void {
+    this.voitureForm = new FormGroup({
+      numeroIdentifiant: new FormControl(this.voiture.numeroIdentifiant, [Validators.required]),
+      numeroSerie: new FormControl(this.voiture.numeroSerie, [Validators.required]),
+      dateAchat: new FormControl(this.voiture.dateAchat, [Validators.required]),
+      couleur: new FormControl(this.voiture.couleur, [Validators.required]),
+      marque: new FormControl('', [Validators.required]),
+      modele: new FormControl('', [Validators.required]),
+    })
+    this.listesVoitures();
+    this.listesMarques();
+    this.listesModeles()
+  }
+
+  changeModele(e: any) {
+    this.modele?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+  }
+
+  changeMarque(e: any) {
+    this.marque?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+  }
+
+  //Méthode de la liste de toutes les marques à partir de VoitureService
+  listesMarques():void{
+    this.marqueService.getAll().subscribe(response=>{
+      this.marques = response;
+    })
+  }
+
+  //Méthode de la liste de tous les modèles à partir de VoitureService
+  listesModeles():void{
+    this.modeleService.getAll().subscribe(response=>{
+      this.modeles = response;
+    })
+  }
+  //Méthode de la liste de toutes les voitures à partir de VoitureService
+  listesVoitures():void{
+    this.voitureService.getAll().subscribe(response=>{
+      this.voitures = response;
+    })
+  }
+
+  afficherFormulaireAjouter(): void {
+    this.voirListesVoitures = false;
+    this.voirFormulaireAjout = true;
+    this.voirFormulaireModification;
+    this.voiture = new Voiture();
+  }
+
+  get numeroIdentifiant(){
+    return this.voitureForm.get('numeroIdentifiant');
+  }
+
+  get numeroSerie(){
+    return this.voitureForm.get('numeroSerie');
+  }
+
+  get dateAchat(){
+    return this.voitureForm.get('dateAchat');
+  }
+
+  get couleur(){
+    return this.voitureForm.get('couleur');
+  }
+
+  get marque(){
+    return this.voitureForm.get('marque');
+  }
+
+  get modele(){
+    return this.voitureForm.get('modele');
+  }
+
+  //Méthode de retour sur la page de la liste des voitures
+  retour(): void {
+    this.voirListesVoitures = true;
+    this.voirFormulaireAjout = false;
+    this.voirFormulaireModification = true;
+    this.voirPageDetail = true;
+    this.erreur = true;
+  }
+
+  //Méthode d'ajout d'une voiture à partir de VoitureService
+  ajouterVoiture(): void {
+    this.voitureService.addVoiture(this.voiture).subscribe(
+      (response) =>{
+        console.log(response);
+        if(response.id > 0) {
+          this.voitures.push({
+            id: response.id,
+            numeroIdentifiant: response.numeroIdentifiant,
+            numeroSerie: response.numeroSerie,
+            dateAchat: response.dateAchat,
+            couleur: response.couleur,
+            statut: response.statut,
+            marque: response.marque,
+            modele: response.modele
+          });
+          this.retour();
+        }
+        else{
+          this.erreur = false;
+          this.messageErreur = "Erreur lors de l'ajout, voiture déjà existante"
+          this.afficherFormulaireAjouter();
+          this.voiture.numeroIdentifiant = response.numeroIdentifiant;
+          this.voiture.numeroSerie = response.numeroSerie;
+          this.voiture.dateAchat = response.dateAchat;
+          this.voiture.couleur = response.couleur;
+          this.voiture.marque = response.marque;
+          this.voiture.modele = response.modele
+        }
+    },
+    (error) =>{
+      console.log(error)
+    })
   }
 
 }
